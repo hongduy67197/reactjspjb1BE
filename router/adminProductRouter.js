69fa27a56742055c896754748a731f1b31990eda
hongduy67197 <destinyoblivion97@gmail.com>
2022-05-19 23:51:16 +0700

    19-5,1

--- router/adminProductRouter.js
+++ router/adminProductRouter.js
@@ -5,7 +5,7 @@ const multer = require('multer')
 const path = require('path')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './views')
+        cb(null, './views/assets/img/productPic')
     },
     filename: function (req, file, cb) {
         cb(
