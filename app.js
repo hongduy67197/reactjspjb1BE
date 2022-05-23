802eace7ffddb6ca9b549bf004c2bbba8dc85500
hongduy67197 <destinyoblivion97@gmail.com>
2022-05-23 10:29:35 +0700

    cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const Router = require('./router')
 app.use('/views', express.static(path.join(__dirname, './views')))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors())
 app.use('/', Router)
 app.listen(port, () => {
     console.log(`serve listen in localhost ${port}`);
