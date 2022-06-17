8b897fbe5bf19decc8b2ea3465bbea454c655c92
hongduy67197 <destinyoblivion97@gmail.com>
2022-06-17 11:52:19 +0700

    master-17-6-1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cookie = require("cookie-parser");
 
 app.set('view engine', 'ejs')
 app.use("/views", express.static(path.join(__dirname, "./views")));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(cookie());
